19f74fb94d12af08cdf3ea606aba2bda6221ed3b
Seanba <sean@seanba.com>
2025-08-01 12:00:09 -0500

    next meeting script working to spec

--- scripts/post-next-meeting.py
+++ scripts/post-next-meeting.py
@@ -1,8 +1,21 @@
 from icalendar import Calendar
+import fileinput
 import requests
 
 ics = requests.get('https://www.meetup.com/Oak-Hill-Toastmasters/events/ical/');
 calendar = Calendar.from_ical(ics.text)
+
+def replace_next_meeting_line(month, day, year, url):
+    print(f"Next meeting: {month} {day}, {year} ({url})")
+    file_path = "..\index.html"
+    with fileinput.input(file_path, inplace=True) as file:
+        for line in file:
+            if "next-meeting-ref" in line:
+                num_spaces = len(line) - len(line.lstrip(' '))
+                a_tag = f'<a id="next-meeting-ref" href="{url}">Next meeting: {month} {day}, {year}</a>\n'
+                print(num_spaces * " " + a_tag, end='')
+            else:
+                print(line, end='')
 
 for event in calendar.walk('VEVENT'):
     status = event.get('STATUS')
@@ -13,10 +26,7 @@ for event in calendar.walk('VEVENT'):
         day = date.dt.day
         year = date.dt.year
 
-        # Now replace the line of HTML for our next meeting link in Meetup
-
-        print(f"  Next confirmed event: {month} {day}, {year}")
-        print(f"  url: {url}")
+        replace_next_meeting_line(month, day, year, url)
         break
 
 
